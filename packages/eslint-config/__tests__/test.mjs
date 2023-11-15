35b4260e0c9c146a15bd8115e9eb12d8e6bfef90
Jeff <rustyangel@gmail.com>
2023-11-15 12:00:39 -0700

    Add changesets

--- packages/eslint-config/__tests__/test.mjs
+++ packages/eslint-config/__tests__/test.mjs
@@ -1,23 +1,28 @@
-// Import { createRequire } from 'node:module'
+import { createRequire } from 'node:module'
 
 import test from 'ava'
-// Import { ESLint } from 'eslint'
-// import isPlainObj from 'is-plain-obj'
+import { ESLint } from 'eslint'
+import isPlainObj from 'is-plain-obj'
 
-// const require = createRequire(import.meta.url)
-// const hasRule = (errors, ruleId) =>
-// 	errors.some((error) => error.ruleId === ruleId)
+const require = createRequire(import.meta.url)
+const hasRule = (errors, ruleId) =>
+	errors.some((error) => error.ruleId === ruleId)
 
-// async function runEslint(string, config) {
-// 	let eslint = new ESLint({
-// 		useEslintrc: false,
-// 		overrideConfig: config,
-// 	})
+async function runEslint(string, config) {
+	let eslint = new ESLint({
+		useEslintrc: false,
+		overrideConfig: {
+			...config,
+			rules: {
+				'unicorn/expiring-todo-comments': 'off',
+			},
+		},
+	})
 
-// 	let [firstResult] = await eslint.lintText(string)
+	let [firstResult] = await eslint.lintText(string)
 
-// 	return firstResult.messages
-// }
+	return firstResult.messages
+}
 
 test('no op', async (t) => {
 	/**
@@ -27,35 +32,35 @@ test('no op', async (t) => {
 	t.true(true)
 })
 
-// Test('main', async (t) => {
-// 	let config = require('../index.js')
+test.skip('main', async (t) => {
+	let config = require('../index.js')
 
-// 	t.true(isPlainObj(config))
-// 	t.true(isPlainObj(config.rules))
+	t.true(isPlainObj(config))
+	t.true(isPlainObj(config.rules))
 
-// 	let errors = await runEslint(
-// 		"'use strict';\nfunction q() { const foo = 'FOO' }\n",
-// 		config,
-// 	)
-// 	t.true(hasRule(errors, 'prefer-let/prefer-let'), JSON.stringify(errors))
-// })
+	let errors = await runEslint(
+		"'use strict';\nfunction q() { const foo = 'FOO' }\n",
+		config,
+	)
+	t.true(hasRule(errors, 'prefer-let/prefer-let'), JSON.stringify(errors))
+})
 
-// test('node', async (t) => {
-// 	let config = require('../node.js')
+test.skip('node', async (t) => {
+	let config = require('../node.js')
 
-// 	t.true(isPlainObj(config))
-// 	t.true(isPlainObj(config.rules))
+	t.true(isPlainObj(config))
+	t.true(isPlainObj(config.rules))
 
-// 	let errors = await runEslint(
-// 		"import path from 'path'\nimport foo from './utils/foo'\n",
-// 		{
-// 			parserOptions: { sourceType: 'module', ecmaVersion: 2020 },
-// 			...config,
-// 		},
-// 	)
-// 	/** @todo Figure out why this rule isn't being caught in tests. */
-// 	t.false(
-// 		hasRule(errors, 'n/file-extension-in-import'),
-// 		JSON.stringify(errors),
-// 	)
-// })
+	let errors = await runEslint(
+		"import path from 'path'\nimport foo from './utils/foo'\n",
+		{
+			parserOptions: { sourceType: 'module', ecmaVersion: 2020 },
+			...config,
+		},
+	)
+	/** @todo Figure out why this rule isn't being caught in tests. */
+	t.false(
+		hasRule(errors, 'n/file-extension-in-import'),
+		JSON.stringify(errors),
+	)
+})
