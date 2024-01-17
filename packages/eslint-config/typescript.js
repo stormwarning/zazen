553aaa6927bae0966a3d43f5b48147efe8067dc6
Jeff <rustyangel@gmail.com>
2024-01-17 12:33:43 -0700

    Add rules for consistent TypeScript import style (#183)

--- packages/eslint-config/typescript.js
+++ packages/eslint-config/typescript.js
@@ -12,8 +12,35 @@ const config = {
 	 */
 	extends: ['xo-typescript', 'plugin:etc/recommended', 'prettier'],
 	rules: {
+		/**
+		 * Used in conjunction with `no-import-type-side-effects` and
+		 * `import/no-duplicates` for TypeScript import style.
+		 *
+		 * @see https://typescript-eslint.io/rules/consistent-type-imports
+		 */
+		'@typescript-eslint/consistent-type-imports': [
+			'error',
+			{ prefer: 'type-imports' },
+		],
+
+		/**
+		 * Used in conjunction with `consistent-type-imports` and
+		 * `import/no-duplicates` for TypeScript import style.
+		 *
+		 * @see https://typescript-eslint.io/rules/no-import-type-side-effects
+		 */
+		'@typescript-eslint/no-import-type-side-effects': 'error',
+
+		/**
+		 * Disabled in `eslint-plugin-import` `typescript` config.
+		 * @see https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/named.md
+		 */
 		'import/named': 'off',
 
+		/**
+		 * Organise comparisons left-to-right from lower to higher.
+		 * @see https://github.com/cartant/eslint-plugin-etc/blob/main/docs/rules/prefer-less-than.md
+		 */
 		'etc/prefer-less-than': 'error',
 	},
 }
