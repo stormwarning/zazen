d75ca86e01f3f49f2a8832a7ffa3964d726d8efb
Jeff <rustyangel@gmail.com>
2024-06-17 13:13:34 -0600

    Override options for CSS and Sass code

---
+++ .changeset/fresh-lamps-tickle.md
@@ -0,0 +1,7 @@
+---
+'@zazen/prettier-config': minor
+---
+
+Override options for CSS and Sass code
+
+Increases `printWidth` and uses double quotes, following the `@stylistic` Stylelint conventions.
--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -4,10 +4,25 @@ const config = {
 	semi: false,
 	singleQuote: true,
 	overrides: [
+		/**
+		 * Give TypeScript code a little more room for type annotations.
+		 */
 		{
 			files: ['*.ts', '*.tsx'],
 			options: {
 				printWidth: 100,
+			},
+		},
+
+		/**
+		 * Follow conventions from @stylistic/stylelint-config.
+		 * @see https://github.com/stylelint-stylistic/stylelint-config/blob/main/.stylelintrc.json
+		 */
+		{
+			files: ['*.css', '*.scss'],
+			options: {
+				singleQuote: false,
+				printWidth: 120,
 			},
 		},
 	],
