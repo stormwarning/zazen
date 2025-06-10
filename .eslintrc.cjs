d141bb3e346b1eb14f89e2828f4fe4fc36719ac8
Jeff <rustyangel@gmail.com>
2025-06-10 15:18:15 -0600

    Remove explicit editorconfig JSON & YML options

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,8 +18,17 @@ const config = {
 		'import/no-anonymous-default-export': ['error', { allowObject: true }],
 	},
 	overrides: [
+		{ files: ['*.cjs'], env: { node: true } },
+
 		{
 			files: '**/__tests__/**/*.js',
+			rules: {
+				'import/no-extraneous-dependencies': 'off',
+			},
+		},
+
+		{
+			files: '**/*.config.js',
 			rules: {
 				'import/no-extraneous-dependencies': 'off',
 			},
@@ -29,6 +38,21 @@ const config = {
 			files: 'packages/eslint-config/*.js',
 			rules: {
 				'unicorn/prefer-module': 'off',
+			},
+		},
+
+		{
+			files: '**/*.ts',
+			extends: ['@zazen/eslint-config/typescript'],
+			parserOptions: {
+				project: './jsconfig.json',
+			},
+			rules: {
+				'@typescript-eslint/ban-types': 'off',
+				'@typescript-eslint/lines-between-class-members': 'off',
+				'@typescript-eslint/padding-line-between-statements': 'off',
+
+				'import/extensions': ['off'],
 			},
 		},
 	],
---
+++ prettier.config.js
@@ -0,0 +1,1 @@
+export { default } from '@zazen/prettier-config'
